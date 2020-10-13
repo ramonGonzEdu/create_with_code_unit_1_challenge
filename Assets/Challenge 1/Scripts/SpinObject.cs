a8c66f146e37a7a574ca26f24d47071da0f88744
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-10-13 08:15:37 -0400

    spinny nose stick

---
+++ Assets/Challenge 1/Scripts/SpinObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+	public enum Vects
+	{
+		forward,
+		right,
+		up,
+		custom,
+	}
+
+
+	public Vects rotationVectorChoice = Vects.forward;
+	public Vector3 rotationVector = Vector3.forward;
+	public float rotationSpeed = 5.0f;
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		switch (rotationVectorChoice)
+		{
+			case Vects.forward:
+				transform.Rotate(Vector3.forward, rotationSpeed);
+				break;
+			case Vects.right:
+				transform.Rotate(Vector3.right, rotationSpeed);
+				break;
+			case Vects.up:
+				transform.Rotate(Vector3.up, rotationSpeed);
+				break;
+			case Vects.custom:
+				transform.Rotate(rotationVector, rotationSpeed);
+				break;
+			default: break;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 * ~~Plane that moves extremely fast~~ Plane that moves at a *niceer* speed.
 * ~~Plane tilts by itself using an *advanced* AI~~ Player controls tilt of plane.
 * ~~Camera in front of the plane so you can see yourself ;)~~ Camera beside the plane so you see yourself crash.
-* Sad Lame Stationary nose stick
+* ~~Sad Lame Stationary nose stick~~ Cool new *spinny* nose stick.
